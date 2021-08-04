329d1cb4a1e706167c1a06c10a329ee8da9cf58d
Emily Read <emilyread01@gmail.com>
2021-08-03 17:27:25 -0700

    Initial commit

---
+++ Zookeeper/src/com/emilyread/zookeeper/GorrillaTest.java
@@ -0,0 +1,20 @@
+package com.emilyread.zookeeper;
+
+public class GorrillaTest {
+
+	public static void main(String[] args) {
+		
+		Gorilla g= new Gorilla();
+		
+		g.throwSomething();
+		g.throwSomething();
+		g.throwSomething();
+		
+		g.eatBanana();
+		g.eatBanana();
+		
+		g.climb();
+		
+	}
+
+}
---
+++ Zookeeper/src/com/emilyread/zookeeper/Mammal.java
@@ -0,0 +1,15 @@
+package com.emilyread.zookeeper;
+
+public class Mammal {
+	public int energy;
+	
+	public Mammal() {
+		this.energy=100;
+		
+	}
+	
+	public int displayEnergy() {
+		return this.energy;
+	}
+
+}
